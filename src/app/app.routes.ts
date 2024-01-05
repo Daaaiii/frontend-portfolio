4b30e23987bf99ce7dfce11caf682aac83ed6148
Daiane Bolzan <dainha85@gmail.com>
2024-01-05 14:15:58 -0300

    feat: add about compoenent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { HabilitiesComponent } from './components/habilities/habilities.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 export const routes: Routes = [
@@ -19,5 +20,9 @@ export const routes: Routes = [
     path: 'habilities',
    component: HabilitiesComponent
   },
-  
+  {
+    path: 'about',
+   component: AboutComponent
+  },
+
 ];
