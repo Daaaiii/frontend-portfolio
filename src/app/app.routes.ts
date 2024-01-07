5bbb88b8cd0f5c20798fe26f3db4c86e2734748a
Daiane Bolzan <dainha85@gmail.com>
2024-01-06 23:18:49 -0300

    fix: remove unused imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { HabilitiesComponent } from './components/habilities/habilities.component';
-import { AboutComponent } from './components/about/about.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
